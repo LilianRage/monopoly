e3b8e323b3a1b1578ed70c2390a1c43e375a2e2a
LilianRage <92574996+LilianRage@users.noreply.github.com>
2022-01-08 17:20:26 +0100

    Create Propriete.h

---
+++ Propriete.h
@@ -0,0 +1,12 @@
+#ifndef MONOPPLATEAU_PROPRIETE_H
+#define MONOPPLATEAU_PROPRIETE_H
+
+typedef struct {
+    int id;
+    int idCase;
+    int price;
+    int owner;
+    int rent;
+}Propriete;
+
+#endif //MONOPPLATEAU_PROPRIETE_H
